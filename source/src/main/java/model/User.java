ba79f0a30c5940511ee85759b691b30c226f7ab6
plusdojo2025-wada <wada-madoka-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-10 13:57:11 +0900

    Update User.java

--- source/src/main/java/model/User.java
+++ source/src/main/java/model/User.java
@@ -7,27 +7,29 @@ public class User implements Serializable {
 	private String password;
 	private String email;
 	
+	
+	//引数があるコンストラクタ
 	public User(String password, String email) {
 		this.password = password;
 		this.email = email;
 	}
-	
+	//引数がないコンストラクタ
 	public User() {
 		this("","");
 	}
-//getta setta
+	//	パスワードのgetta 
 	public String getPassword() {
 		return password;
 	}
-
+	//パスワードのsetta
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	//emailのgetta
 	public String getEmail() {
 		return email;
 	}
-
+	//emailの
 	public void setEmail(String email) {
 		this.email = email;
 	}
