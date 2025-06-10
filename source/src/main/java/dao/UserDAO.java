d9cd6c44fe94246a6e0a266b8d0360d2bc676a28
plusdojo2025-shigeta <shigeta-reo-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-10 15:14:21 +0900

    Update UserDAO

--- source/src/main/java/dao/UserDAO.java
+++ source/src/main/java/dao/UserDAO.java
@@ -1,10 +1,33 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import lib.DbConnection;
+import model.User;
+
 public class UserDAO {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+	public boolean isRegisteredUser(User user) {
+		String sql = "SELECT COUNT(*) AS count FROM user WHERE email = ? AND password = ?";
+		try (Connection conn = DbConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
+			pstmt.setString(1, user.getEmail());
+			pstmt.setString(2, user.getPassword());
+
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (rs.next()) {
+					return rs.getInt("count") == 1;
+				}
+			}
+		} catch (SQLException e) {
+			System.err.println("ユーザー認証時にエラーが発生しました: " + e.getMessage());
+			e.printStackTrace();
+		}
+
+		return false;
 	}
 
 }
