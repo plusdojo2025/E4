30b8ff7e80ea374a9c55b4ee5910ec12d69e30d8
plusdojo2025-wada <wada-madoka-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-10 09:48:14 +0900

    model、DAO、liファイル作成

---
+++ source/src/main/java/lib/Connection.java
@@ -0,0 +1,10 @@
+package lib;
+
+public class Connection {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ source/src/main/java/model/Rewards.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Rewards {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
