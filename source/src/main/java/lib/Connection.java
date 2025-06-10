3e3e6f316305e535db4971104f3c02128a324373
plusdojo2025-shigeta <shigeta-reo-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-10 13:18:09 +0900

    Update lib/connection

--- source/src/main/java/lib/Connection.java
+++ source/src/main/java/lib/Connection.java
@@ -1,10 +1,23 @@
 package lib;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Connection {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/e4?"
+            + "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true&allowPublicKeyRetrieval=true";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "password";
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("JDBCドライバのロードに失敗しました", e);
+        }
+    }
 
-	}
-
+    public static java.sql.Connection conn() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASS);
+    }
 }
