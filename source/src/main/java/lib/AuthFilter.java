571dab096f3f11717b72caab69e1f786f0b02ae8
plusdojo2025-shigeta <shigeta-reo-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-10 12:38:04 +0900

    Update AuthFilter

--- source/src/main/java/lib/AuthFilter.java
+++ source/src/main/java/lib/AuthFilter.java
@@ -1,10 +1,47 @@
-package lib;
-
-public class AuthFilter {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
-
-}
+//package lib;
+//
+//import java.io.IOException;
+//
+//import javax.servlet.Filter;
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.ServletRequest;
+//import javax.servlet.ServletResponse;
+//import javax.servlet.annotation.WebFilter;
+//import javax.servlet.http.HttpFilter;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//
+///**
+// * Servlet Filter implementation class AuthFilter
+// */
+//@WebFilter("/*")
+//public class AuthFilter extends HttpFilter implements Filter {
+//
+//	/**
+//	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+//	 */
+//	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//			throws IOException, ServletException {
+//
+//		HttpServletRequest httpRequest = (HttpServletRequest) request;
+//		HttpServletResponse httpResponse = (HttpServletResponse) response;
+//
+//		String contextPath = httpRequest.getContextPath();
+//		String requestURI = httpRequest.getRequestURI();
+//
+//		HttpSession session = httpRequest.getSession(false);
+//		boolean isLoggedIn = (session != null && session.getAttribute("user") != null);
+//
+//		boolean isLoginRequest = requestURI.equals(contextPath + "/login.html");
+//		boolean isAuthChangerRequest = requestURI.equals(contextPath + "/AuthChanger");
+//		boolean isStyleSheet = requestURI.equals(contextPath + "/style.css");
+//
+//		if (isLoggedIn || isLoginRequest || isAuthChangerRequest || isStyleSheet) {
+//			chain.doFilter(request, response);
+//		} else {
+//			httpResponse.sendRedirect(contextPath + "/login.html");
+//		}
+//	}
+//}
