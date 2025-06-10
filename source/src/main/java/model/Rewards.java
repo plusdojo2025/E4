5f6a05afbfa4bb8e34fe42caee2995f49e01dac5
plusdojo2025-yamasaki <yamasaki-kanane-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-10 14:01:09 +0900

    Update Rewards.java

--- source/src/main/java/model/Rewards.java
+++ source/src/main/java/model/Rewards.java
@@ -1,10 +1,40 @@
 package model;
 
-public class Rewards {
+import java.io.Serializable;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+public class Rewards implements Serializable {
+	private String gacha_low;
+	private String gacha_mid;
+	private String gacha_high;
+	
+	public Rewards(String gacha_low, String gacha_mid, String gacha_high) {
+		this.gacha_low = gacha_low;
+		this.gacha_mid = gacha_mid;
+		this.gacha_high = gacha_high;
 	}
 
-}
+	public String getGacha_low() {
+		return gacha_low;
+	}
+
+	public void setGacha_low(String gacha_low) {
+		this.gacha_low = gacha_low;
+	}
+
+	public String getGacha_mid() {
+		return gacha_mid;
+	}
+
+	public void setGacha_mid(String gacha_mid) {
+		this.gacha_mid = gacha_mid;
+	}
+
+	public String getGacha_high() {
+		return gacha_high;
+	}
+
+	public void setGacha_high(String gacha_high) {
+		this.gacha_high = gacha_high;
+	}
+
+	}
