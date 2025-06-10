ded85ac1dab4f03b0f92de299b2a960f452ad632
plusdojo2025-shigeta <shigeta-reo-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-10 15:24:24 +0900

    Update UserDAO

--- source/src/main/java/dao/UserDAO.java
+++ source/src/main/java/dao/UserDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import lib.DbConnection;
 import model.User;
@@ -29,5 +30,50 @@ public class UserDAO {
 
 		return false;
 	}
+	
+	public User insertUser(User user) {
+		String sql = "INSERT INTO user (email, password) VALUES (?, ?)";
+		try (Connection conn = DbConnection.getConnection();
+				PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
+			ps.setString(1, user.getEmail());
+			ps.setString(2, user.getPassword());
+
+
+			int affectedRows = ps.executeUpdate();
+
+			if (affectedRows == 0) {
+				throw new SQLException("ユーザー登録に失敗しました。");
+			}
+
+			try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+				if (generatedKeys.next()) {
+					return selectById(generatedKeys.getInt(1));
+				} else {
+					throw new SQLException("IDの取得に失敗しました。");
+				}
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private User selectById(int id) {
+		String sql = "SELECT email, password FROM user WHERE id = ?";
+		try (Connection conn = DbConnection.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+			pstmt.setInt(1, id);
+
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (rs.next()) {
+					return new User(rs.getString("password"),rs.getString("email"));
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
