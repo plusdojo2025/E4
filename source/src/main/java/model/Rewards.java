5e4c46267d18dd84995dc836fc01d8d8d5271837
plusdojo2025-yamasaki <yamasaki-kanane-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-10 14:29:59 +0900

    Update Rewards.java

--- source/src/main/java/model/Rewards.java
+++ source/src/main/java/model/Rewards.java
@@ -1,47 +1,55 @@
 package model;
 
 import java.io.Serializable;
+import java.sql.Date;
 
 public class Rewards implements Serializable {
 	private int user_id;
-	private String gacha_time;
+	private Date gacha_time;
 	private static int gacha_type;
-	private String gacha_number;
-	
-	public Rewards() {
-		this(gacha_type, "", gacha_type, "");
-	}
-	
-	public Rewards(int user_id, String gacha_time, int gacha_type, String gacha_number) {
+	private int gacha_number;
 
+	public Rewards() {
+		this.user_id = 0;
+		this.gacha_number = 0;
+		this.gacha_time = new Date(System.currentTimeMillis());
+	}
+
+	public Rewards(int user_id, Date gacha_time, int gacha_number) {
 		this.user_id = user_id;
 		this.gacha_time = gacha_time;
-		this.gacha_type = gacha_type;
 		this.gacha_number = gacha_number;
 	}
-	
+
 	public int getUser_id() {
 		return user_id;
 	}
+
 	public void setUser_id(int user_id) {
 		this.user_id = user_id;
 	}
-	public String getGacha_time() {
+
+	public Date getGacha_time() {
 		return gacha_time;
 	}
-	public void setGacha_time(String gacha_time) {
+
+	public void setGacha_time(Date gacha_time) {
 		this.gacha_time = gacha_time;
 	}
-	public int getGacha_type() {
+
+	public static int getGacha_type() {
 		return gacha_type;
 	}
-	public void setGacha_type(int gacha_type) {
-		this.gacha_type = gacha_type;
+
+	public static void setGacha_type(int gacha_type) {
+		Rewards.gacha_type = gacha_type;
 	}
-	public String getGacha_number() {
+
+	public int getGacha_number() {
 		return gacha_number;
 	}
-	public void setGacha_number(String gacha_number) {
+
+	public void setGacha_number(int gacha_number) {
 		this.gacha_number = gacha_number;
 	}
 }
