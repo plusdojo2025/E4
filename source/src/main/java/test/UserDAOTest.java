2614eedb76a55d5766066a0d39a3e8fbed8d7b4c
plusdojo2025-shigeta <shigeta-reo-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-11 14:29:54 +0900

    Add gson for test

--- source/src/main/java/test/UserDAOTest.java
+++ source/src/main/java/test/UserDAOTest.java
@@ -1,11 +1,15 @@
 package test;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import dao.UserDAO;
 import model.User;
 
 public class UserDAOTest {
 
 	public static void main(String[] args) {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		UserDAO dao = new UserDAO();
 		User correctUser = new User("test@example.com", "password");
 		System.out.println("正しいユーザーの認証:" + dao.isRegisteredUser(correctUser));
@@ -18,6 +22,6 @@ public class UserDAOTest {
 
 		User insertUser = new User("hogehoge@example.com", "woonyannn");
 		User insertedUser = dao.insertUser(insertUser);
-		System.out.println("新規ユーザーの登録:" + insertedUser.getEmail() + " : " + insertedUser.getPassword());
+		System.out.println("新規ユーザーの登録:" +gson.toJson(insertedUser));
 	}
 }
