665c1c276e31cb44fd8f97f6d5b57010e53f5c36
plusdojo2025-fujino <fujino-atsuhiro-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-11 14:55:57 +0900

    Update SignUpServlet.java

--- source/src/main/java/servlet/SignUpServlet.java
+++ source/src/main/java/servlet/SignUpServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.UserDAO;
+import model.User;
+
 @WebServlet("/SignUpServlet")
 public class SignUpServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -18,4 +21,31 @@ public class SignUpServlet extends HttpServlet {
             throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/jsp/sign_up.jsp").forward(request, response);
     }
+    
+    // 新規登録処理
+ 	@Override
+ 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+ 			throws ServletException, IOException {
+
+ 		
+ 		// パラメータ取得
+ 		request.setCharacterEncoding("UTF-8");
+ 		String email = request.getParameter("email");
+ 		String password = request.getParameter("password");
+ 		
+ 	// ユーザー登録処理
+ 			UserDAO dao = new UserDAO();
+ 			User registeredUser = dao.insertUser(new User(email, password));
+
+ 			if (registeredUser != null) {
+ 				// 登録成功 → 成功メッセージを表示
+ 				//request.setAttribute("resultTitle", "登録成功");
+ 				request.setAttribute("nextUrl", "/webapp/LoginServlet");
+ 			} else {
+ 				// 登録失敗（たとえばすでに同じemailがあるなど）
+ 				//request.setAttribute("resultTitle", "登録失敗");
+ 				request.setAttribute("nextUrl", "/webapp/SignUpServlet");
+ 			}
+ 	}
+
 }
