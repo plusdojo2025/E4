efc40ef72ab74f5ad79fbcbb31163c633dba9e94
plusdojo2025-fujino <fujino-atsuhiro-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 15:41:33 +0900

    AccountServlet更新

--- source/src/main/java/servlet/AccountServlet.java
+++ source/src/main/java/servlet/AccountServlet.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import model.User;
-
 @WebServlet("/AccountServlet")
 public class AccountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -20,6 +18,9 @@ public class AccountServlet extends HttpServlet {
 
 		// セッションの取得（ログイン情報の確認）
 		HttpSession session = request.getSession(false);
+		
+		int userId = 1; // テスト用に仮置き
+/*		
 		if (session == null || session.getAttribute("user") == null) {
 			// セッションなしはログインページへ
 			response.sendRedirect("/E4/LoginServlet");
@@ -32,7 +33,7 @@ public class AccountServlet extends HttpServlet {
 
 		// メールアドレスをリクエストスコープに格納
 		request.setAttribute("email", email);
-
+*/
 		// アカウント情報表示ページへフォワード
 		request.getRequestDispatcher("/WEB-INF/jsp/account.jsp").forward(request, response);
 	}
