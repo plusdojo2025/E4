ee1ba80b72f00cb93242ab18e343bf94d784bb45
plusdojo2025-shigeta <shigeta-reo-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-16 17:44:43 +0900

    コードのパス情報からE４を全部消す

--- source/src/main/java/servlet/SignUpServlet.java
+++ source/src/main/java/servlet/SignUpServlet.java
@@ -13,39 +13,38 @@ import model.User;
 
 @WebServlet("/SignUpServlet")
 public class SignUpServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    // 新規登録画面を表示
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/jsp/sign_up.jsp").forward(request, response);
-    }
-    
-    // 新規登録処理
- 	@Override
- 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
- 			throws ServletException, IOException {
+	// 新規登録画面を表示
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/jsp/sign_up.jsp").forward(request, response);
+	}
 
- 		
- 		// パラメータ取得
- 		request.setCharacterEncoding("UTF-8");
- 		String email = request.getParameter("email");
- 		String password = request.getParameter("password");
- 		
- 	// ユーザー登録処理
- 			UserDAO dao = new UserDAO();
- 			User registeredUser = dao.insertUser(new User(email, password));
+	// 新規登録処理
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
- 			if (registeredUser != null) {
- 				// 登録成功 → 成功メッセージを表示
- 				//request.setAttribute("resultTitle", "登録成功");
- 				request.setAttribute("nextUrl", "/E4/LoginServlet");
- 			} else {
- 				// 登録失敗（たとえばすでに同じemailがあるなど）
- 				//request.setAttribute("resultTitle", "登録失敗");
- 				request.setAttribute("nextUrl", "/E4/SignUpServlet");
- 			}
- 	}
+		// パラメータ取得
+		request.setCharacterEncoding("UTF-8");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		// ユーザー登録処理
+		UserDAO dao = new UserDAO();
+		User registeredUser = dao.insertUser(new User(email, password));
+
+		if (registeredUser != null) {
+			// 登録成功 → 成功メッセージを表示
+			// request.setAttribute("resultTitle", "登録成功");
+			request.setAttribute("nextUrl", request.getContextPath() + "/LoginServlet");
+		} else {
+			// 登録失敗（たとえばすでに同じemailがあるなど）
+			// request.setAttribute("resultTitle", "登録失敗");
+			request.setAttribute("nextUrl", request.getContextPath() + "/SignUpServlet");
+		}
+	}
 
 }
